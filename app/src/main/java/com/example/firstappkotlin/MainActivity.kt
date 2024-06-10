8c7269665b2b2aa7639b2793f2f0c0e079c53d06
GamerBoiGleet <hsmotinge04@gmail.com>
2024-06-10 16:36:14 +0530

    This is my first commit

---
+++ app/src/main/java/com/example/firstappkotlin/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.firstappkotlin
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("ResourceAsColor")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val coolbutton = findViewById<Button>(R.id.btnCool)
+        val hotbutton = findViewById<Button>(R.id.btnHot)
+        val linearLayout = findViewById<LinearLayout>(R.id.linlay)
+
+        coolbutton.setOnClickListener {
+            linearLayout.setBackgroundColor(R.color.blue)
+        }
+
+        hotbutton.setOnClickListener {
+            linearLayout.setBackgroundColor(R.color.red)
+        }
+
+        }
+    }
